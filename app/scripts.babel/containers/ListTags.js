5f19c8526fd5b6842d2f2ade78249b326cd12e82
Khang Hoang <hoangtrieukhang@gmail.com>
2016-07-28 21:26:17 +0700

    open popup to login with github

--- app/scripts.babel/containers/ListTags.js
+++ app/scripts.babel/containers/ListTags.js
@@ -1,5 +1,5 @@
 import '../../styles/github.css';
-import { Component } from 'react';
+import React, { Component } from 'react';
 import list from '../components/List';
 import {
   GET_REPOS_BY_TAG_URL as getRepoByTags,
@@ -11,6 +11,8 @@ class ListRepo extends Component {
     this.state = {
       repos: [],
     };
+
+    this.onClick = this.onClick.bind(this);
   }
   componentDidMount() {
     fetch(getRepoByTags('javascript'))
@@ -25,8 +27,31 @@ class ListRepo extends Component {
       })
       .catch(console.log);
   }
+
+  onClick(e) {
+
+    // Prevent default anchor event
+    e.preventDefault();
+    // Set values for window
+    const intWidth = '500';
+    const intHeight = '400';
+    const strResize = 'no';
+
+    // Set title and open popup with focus on it
+    const title = 'Login with github';
+    const strParam = `width=${intWidth},height=${intHeight}',resizable='${strResize}'`;
+    const url = 'http://localhost:3333/auth/github';
+    window.open(url, title, strParam).focus();
+  }
+
   render() {
-    return list(this.state.repos);
+    const listRepos = list(this.state.repos);
+    return (
+      <div>
+        <div onClick={this.onClick}>Login with github</div>
+        {listRepos}
+      </div>
+    );
   }
 }
 
