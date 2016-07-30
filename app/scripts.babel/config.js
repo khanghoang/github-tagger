206e3c83cda034caad272784073d1003707d0566
Khang Hoang <hoangtrieukhang@gmail.com>
2016-07-30 20:36:09 +0700

    fix config file since there's not path package on browser side

--- app/scripts.babel/config.js
+++ app/scripts.babel/config.js
@@ -1,12 +1,10 @@
-import path from 'path';
-
 const BASE_URL = process.env.PRODUCTION ? 'https://github-tagger.herokuapp.com' : 'http://127.0.0.1:3333';
 
-export const GET_REPOS_URL = path.join(BASE_URL, 'getRepo');
+export const GET_REPOS_URL = `${BASE_URL}/getRepo`;
 export const GET_REPOS_BY_TAG_URL = (tags) => (
   `${GET_REPOS_URL}?tags=${tags}`
 );
 
-export const GET_TAGS_URL = path.join(BASE_URL, 'tags');
-export const GITHUB_LOGIN_URL = path.join(BASE_URL, 'auth/github');
-export const SAVE_REPO_URL = path.join(BASE_URL, 'save');
+export const GET_TAGS_URL = `${BASE_URL}/tags`;
+export const GITHUB_LOGIN_URL = `${BASE_URL}/auth/github`;
+export const SAVE_REPO_URL = `${BASE_URL}/save`;
