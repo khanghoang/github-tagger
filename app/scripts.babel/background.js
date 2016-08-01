673b28d8e52e06dc086f5a32c4637afefa31af13
Khang Hoang <hoangtrieukhang@gmail.com>
2016-08-01 20:43:20 +0700

    change badge text base on env variables

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -1,9 +1,12 @@
-'use strict';
+const isProduction = process.env.NODE_ENV === 'production';
+const isLocal = process.env.LOCAL;
 
 chrome.runtime.onInstalled.addListener(details => {
   console.log('previousVersion', details.previousVersion);
 });
 
-chrome.browserAction.setBadgeText({text: '\'Allo'});
+const badgeText = `${isProduction ? 'P' : 'D'}/${isLocal ? 'L' : 'H'}`;
+
+chrome.browserAction.setBadgeText({ text: badgeText });
 
 console.log('\'Allo \'Allo! Event Page for Browser Action');
